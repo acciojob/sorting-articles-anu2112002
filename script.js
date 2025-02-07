d6038e09c8221c161537bdfed64f7a74bd20830d
Anushri <112797477+anu2112002@users.noreply.github.com>
2025-02-07 10:21:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+const bands = [
+            'The Plot in You', 
+            'The Devil Wears Prada', 
+            'Pierce the Veil', 
+            'Norma Jean', 
+            'The Bled', 
+            'Say Anything', 
+            'The Midway State', 
+            'We Came as Romans', 
+            'Counterparts', 
+            'Oh, Sleeper', 
+            'A Skylit Drive', 
+            'Anywhere But Here', 
+            'An Old Dog'
+        ];
+function removeArticles(name) {
+    const articles = ['a', 'an', 'the'];
+    const words = name.split(' ');
+    if (articles.includes(words[0].toLowerCase())) {
+        words.splice(0, 1);
+    }
+    return words.join(' ').trim();
+}
+
+bands.sort((a, b) => {
+	const nameA = removeArticles(a).toLowerCase();
+	const nameB = removeArticles(b).toLowerCase();
+	if (nameA < nameB) {
+		return -1;
+	}
+	if (nameA > nameB) {
+		return 1;
+	}
+	return 0;
+});
+
+const bandList = document.getElementById('band');
+bands.forEach(band => {
+	const li = document.createElement('li');
+	li.textContent = band;
+	bandList.appendChild(li);
+});
